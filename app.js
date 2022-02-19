2400408e2ee5a1f609aade7f1abaadb6ad880093
Chinmay <chinmay11.moghe@gmail.com>
2022-02-19 11:45:05 +0530

    Add wordle class

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log('Hello Wordler!!!');
+"use strict";
+const DEFAULT_WORD_LENGTH = 5;
+const DEFAULT_TRY_COUNT = 6; 
+
+class Wordle {
+    constructor(wordLength=DEFAULT_WORD_LENGTH,tries=DEFAULT_TRY_COUNT) {
+        this.wordLength = wordLength;
+        this.tries = tries;
+        this.gridElement = document.querySelector('.wordle_grid');
+        this.keyBoard = document.querySelector('.keyboard');
+        this.currentWord ='';
+        this.allWords = [];
+        this.currentCount = 0;
+        this.currentTry = 1; 
+    }
+
+    createWordleGrid() {
+        /*create wordle grid and add to DOM*/
+    }
+    
+    createKeyboard() {
+        /*create keyboard and add it to DOM*/
+    }
+    
+    handleKeyPress() {
+        /* handle keypress for keyboard*/
+    }
+
+    handleClickKeyPress() {
+        /*handle click keypress for keyboard*/
+    }
+    
+    handleEnterKey() {
+        /*handle Enter key press*/
+    }
+
+    handleBackSpaceKey() {
+        /*handle backspace key press*/
+    }
+}
+
+const initApp = () => {
+    const wordle = new Wordle();
+}
+
+document.addEventListener("DOMContentLoaded",initApp);
